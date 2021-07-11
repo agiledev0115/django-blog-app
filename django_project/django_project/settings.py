f2f24c7269f5ec6a96da0fbdbd69e7bdf25f69d1
agiledev0115 <alexanderagiledev@gmail.com>
2021-07-11 20:01:30 +0530

    Updated

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # DEBUG = True
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['mydjangobloggingapp.heroku.com']
+ALLOWED_HOSTS = ['<app_name>.heroku.com']
 
 
 # Application definition
